0df8e2b83ccdf6a1baf148b2dbe50887d549d690
guayaba-dev <guayaba.dev@gmail.com>
2025-01-06 08:13:19 -0500

    Model drawing implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
-#include <SDL2/SDL_render.h>
-
-#include <iostream>
+#include <algorithm>
+#include <cstddef>
 #include <utility>
 
 #include "SDL2/SDL.h"
+#include "geometry.h"
+#include "model.h"
 
 struct Color {
   int r;
@@ -14,24 +15,46 @@ struct Color {
 
 void line(int x0, int y0, int x1, int y1, SDL_Renderer* renderer, Color color);
 
-int main() {
+Model* model = NULL;
+Color WHITE = Color(255, 255, 255);
+const int WIDTH = 700;
+const int HEIGHT = 700;
+
+int main(int argc, char** argv) {
+  if (2 == argc) {
+    model = new Model(argv[1]);
+  } else {
+    model = new Model("obj/african_head.obj");
+  }
+
   SDL_Window* window = nullptr;
   SDL_Renderer* renderer = nullptr;
 
   SDL_Init(SDL_INIT_VIDEO);
-  SDL_CreateWindowAndRenderer(100, 100, 0, &window, &renderer);
+  SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer);
 
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
 
-  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-  line(13, 20, 80, 40, renderer, Color(255, 255, 255));
-  line(20, 13, 40, 80, renderer, Color(255, 0, 0));
-  line(80, 40, 13, 20, renderer, Color(255, 0, 0));
+  for (int i = 0; i < model->nfaces(); i++) {
+    std::vector<int> face = model->face(i);
+
+    for (int j = 0; j < 3; j++) {
+      Vec3f v0 = model->vert(face[j]);
+      Vec3f v1 = model->vert(face[(j + 1) % 3]);
+
+      int x0 = (v0.x + 1.) * WIDTH / 2.;
+      int y0 = (v0.y + 1.) * HEIGHT / 2.;
+      int x1 = (v1.x + 1.) * WIDTH / 2.;
+      int y1 = (v1.y + 1.) * HEIGHT / 2.;
+      line(x0, y0, x1, y1, renderer, WHITE);
+    }
+  }
 
   SDL_RenderPresent(renderer);
   SDL_Delay(5000);
 
+  delete model;
   return 0;
 }
 
@@ -59,9 +82,9 @@ void line(int x0, int y0, int x1, int y1, SDL_Renderer* renderer, Color color) {
 
   for (int x = x0; x <= x1; x++) {
     if (step)
-      SDL_RenderDrawPoint(renderer, y, 100 - x);
+      SDL_RenderDrawPoint(renderer, y, HEIGHT - x);
     else
-      SDL_RenderDrawPoint(renderer, x, 100 - y);
+      SDL_RenderDrawPoint(renderer, x, HEIGHT - y);
 
     error2 += derror2;
 
