1a75b99bc3124d9d0c7548474ec627955109ec8c
GuayabaDev <guayaba.dev@gmail.com>
2025-08-22 19:54:52 -0500

    feature: added a realize mode to cmake files now it's way faster than before

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,16 @@
-#include <iostream>
-
 #include "gl.h"
 #include "gl_wrapper.hpp"
-#include "l_matrix.h"
 #include "model.h"
 #include "s_WindowManager.h"
 
-const int HEIGHT = 700;
-const int WIDTH = 700;
-const int DEPTH = 255;
-
-Model* model = NULL;
-Vec3f lighteye(1, 1, 1);
-Vec3f eye(0, 0, 3);
-Vec3f center(0, 0, 0);
-
-zBufferShader shader2 =
-    zBufferShader(lookat(center, lighteye, Vec3f(0, 1, 0)),
-                  viewport(WIDTH, HEIGHT, 0, 0), projection(0));
-
 int main(int argc, char** argv) {
-  if (2 == argc)
-    model = new Model(argv[1]);
-  else
-    model = new Model("obj/african_head.obj");
+  Model* model = NULL;
+  {  // LOADS MODEL
+    if (2 == argc)
+      model = new Model(argv[1]);
+    else
+      model = new Model("obj/african_head.obj");
+  }
 
   Window mainWindow = createWindow();
   setWindow(mainWindow);
@@ -41,15 +28,12 @@ int main(int argc, char** argv) {
 
       if (event.type != SDL_KEYDOWN) continue;
       if (event.key.keysym.sym != SDLK_ESCAPE) continue;
-
-      running = false;
     }
 
     presentWindow(mainWindow);
   }
 
   deleteWindow(mainWindow);
-
   delete model;
   return 0;
 }
